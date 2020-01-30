b420920beeb9a8e9410711dce46c0bceceddba97
Saif Jamal <saifjamal4649@gmail.com>
2020-01-30 02:59:58 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,3 +3,16 @@
 first_number = 5
 second_number = 3
 sum = first_number + second_number
+
+first_number = 5
+second_number = 3
+difference = first_number - second_number
+
+first_number = 5
+second_number = 3
+product = first_number * second_number
+
+first_number = 5
+second_number = 3
+quotient = first_number / second_number
+
